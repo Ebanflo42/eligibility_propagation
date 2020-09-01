9ba1dd3559e435068f6db83dc3934bf450cbbee7
Eben Kadile <eben.kadile24@gmail.com>
2020-09-01 00:53:47 -0700

    messing with fitzhugh nagumo

--- random/fitzhugh_nagumo.py
+++ random/fitzhugh_nagumo.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Fitzhugh-Nagumo vector field
 def vector_field(vw, a, b, tau, I):
 
     v, w = vw[0], vw[1]
@@ -9,27 +10,46 @@ def vector_field(vw, a, b, tau, I):
 
     return vdot, wdot
 
-
+# Generate trajectory
 def simulate(initvw, eps, a, b, tau, noise_fcn, T):
 
+    # spike train is determined by which side of the nullcline the system is on
+    # return whether or not there is a spike and the potential
     def test_w_nullcline(vw):
         d = b*vw[0] - vw[1]
-        #print(d)
         u = d - a
-        if d < 1:
-            return 0
+        if d < 0:
+            return 0, d
         else:
-            return d
+            return 1, d
 
+    # trajectory in 2D phase space
     trajectory = np.zeros((T, 2))
     trajectory[0] = np.array(initvw)
 
+    # potential and spike train
     spike_train = np.zeros(T)
-    spike_train[0] = test_w_nullcline(trajectory[0])
+    potentials = np.zeros(T)
+    spike_train[0], potentials[0] = test_w_nullcline(trajectory[0])
 
+    # Euler approximation
     for t in range(1, T):
         vw = trajectory[t - 1]
         trajectory[t] = vw + eps*np.array(vector_field(vw, a, b, tau, noise_fcn()))
-        spike_train[t] = test_w_nullcline(trajectory[t])
+        spike_train[t], potentials[t] = test_w_nullcline(trajectory[t])
 
-    return trajectory, spike_train
+    # determine firing rate and the number of repeated fires
+    rate, n = 0, 0
+    rate += spike_train[0]
+    for i in range(1, T):
+        rate += spike_train[i]
+        n += spike_train[i]*spike_train[i - 1]
+    rate /= T
+
+    spike_potentials = []
+    for v in potentials:
+        if v > 0:
+            spike_potentials.append(v)
+    v = np.std(spike_potentials)
+
+    return trajectory, spike_train, rate, n, v
